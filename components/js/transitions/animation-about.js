e63901b05d8db8cdb427b12d61858f328f2c9499
LizMSD <lizsolanodavila@gmail.com>
2023-03-30 17:43:19 -0500

    30/03

--- components/js/transitions/animation-about.js
+++ components/js/transitions/animation-about.js
@@ -1,6 +1,7 @@
 
 // animacion para la parte principal
-ScrollReveal().reveal('.contenedor-img', {  delay: 375,
+ScrollReveal().reveal('.contenedor-img', {  
+    delay: 375,
     duration: 1000,
     reset: true,
     origin:'right',
@@ -81,6 +82,70 @@ ScrollReveal().reveal('.design-global', {
   reset:true
 });
 
+//último bloque
 
+ScrollReveal().reveal('.div1', {
+  delay:300,
+  duration: 1100,
+distance:'500px',
+origin:'left',
+reset:true
+});
+
+ScrollReveal().reveal('.div2', {
+  delay:400,
+  duration: 1000,
+distance:"60px",
+reset:true,
+origin: "bottom"
+});
+
+ScrollReveal().reveal('.div3', {
+  delay:550,
+  duration: 1000,
+distance:"60px",
+reset:true,
+origin: "bottom"
+});
+
+ScrollReveal().reveal('.div4', {
+  delay:700,
+  duration: 1000,
+distance:"60px",
+reset:true,
+origin: "bottom"
+});
+
+ScrollReveal().reveal('.div5', {
+  delay:850,
+  duration: 1000,
+distance:"60px",
+reset:true,
+origin: "bottom"
+});
+
+ScrollReveal().reveal('.div6', {
+  delay:1000,
+  duration: 1000,
+distance:"60px",
+reset:true,
+origin: "bottom"
+});
+
+ScrollReveal().reveal('.div7', {
+  delay:1150,
+  duration: 1000,
+distance:"60px",
+reset:true,
+origin: "bottom"
+});
+
+ScrollReveal().reveal('.div8', {
+  delay:1300,
+  duration: 1100,
+distance:'500px',
+origin:'right',
+reset:true
+});
 
 
---
+++ components/js/transitions/animation-soluciones.js
@@ -0,0 +1,69 @@
+ScrollReveal().reveal('.texto-contenedor-s', {
+    delay: 500,
+    duration: 1200,
+  distance:"60px",
+  reset:true,
+  origin: "top"
+  });
+
+ScrollReveal().reveal('.boton-contenedor-s', {
+    delay: 500,
+    duration: 1200,
+  distance:"60px",
+  reset:true,
+  origin: "bottom"
+  });
+
+  ScrollReveal().reveal('.tarjeta', {
+    delay: 600,
+    duration: 1400,
+  distance:"0px",
+  reset:true,
+  origin: "top",
+
+  });
+
+  ScrollReveal().reveal('.tarjeta1', {
+    delay: 800,
+    duration: 1700,
+  distance:"0px",
+  reset:true,
+  origin: "top",
+  });
+
+  ScrollReveal().reveal('.tarjeta2', {
+    delay: 1000,
+    duration: 2000,
+  distance:"0px",
+  reset:true,
+  origin: "top"
+  });
+
+
+  ScrollReveal().reveal('.titulo-planes', {
+    delay: 500,
+    duration: 900,
+  distance:"60px",
+  reset:true,
+  origin: "top"
+  });
+
+
+
+//Cartas 
+// ScrollReveal().reveal('.contenedor-cartas', {
+//     delay: 500,
+//     duration: 1200,
+//   distance:"60px",
+//   reset:true,
+//   origin: "bottom"
+//   });  
+
+//círculo de las cartas
+  ScrollReveal().reveal('.diseño-carta', {
+    delay: 900,
+    duration: 1500,
+  distance:"60px",
+  reset:true,
+  origin: "top"
+  });
