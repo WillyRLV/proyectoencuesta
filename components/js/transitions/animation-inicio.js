306add63c4d6da1023434b69389ebbc5cf1a06d5
Sebastian Caceres <sebastiancaceres207@gmail.com>
2023-09-04 12:08:38 -0500

    Mejorando tiempos de animacion delay y duration - JS

--- components/js/transitions/animation-inicio.js
+++ components/js/transitions/animation-inicio.js
@@ -1,78 +1,78 @@
 ScrollReveal().reveal('.titulo_arriba', {
-    delay: 400,
-    duration: 1200,
+    delay: 300, /*Reduciendo tiempo de delay y duracion*/
+    duration: 1000,
   distance:"60px",
   reset:true,
   origin: "top"
   });
 
   ScrollReveal().reveal('.descripcion_arriba', {
-    delay: 600,
-    duration: 1800,
+    delay: 500,
+    duration: 1600,
   distance:"0px",
   reset:true,
   origin: "top"
   });
 
 ScrollReveal().reveal('.button', {
-    delay: 1100,
-    duration: 2000,
+    delay: 700,
+    duration: 1800,
   distance:"0px",
   reset:true,
   origin: "top"
   });
 
   ScrollReveal().reveal('.cuadro_medio', {
-    delay: 500,
-    duration: 1200,
+    delay: 400,
+    duration: 1000,
   distance:"0px",
   reset:true,
   origin: "bottom"
   });
 
   ScrollReveal().reveal('.cuadro-1', {
-    delay: 800,
-    duration: 1200,
+    delay: 500,
+    duration: 1000,
   distance:"60px",
   reset:true,
   origin: "bottom"
   });
 
   ScrollReveal().reveal('.cuadro-2', {
-    delay: 1100,
-    duration: 1200,
+    delay: 500,
+    duration: 1000,
   distance:"60px",
   reset:true,
   origin: "bottom"
   });
 
   ScrollReveal().reveal('.cuadro-3', {
-    delay: 1400,
-    duration: 1200,
+    delay: 500,
+    duration: 1000,
   distance:"60px",
   reset:true,
   origin: "bottom"
   });
 
   ScrollReveal().reveal('.cuadro-4', {
-    delay: 1700,
-    duration: 1200,
+    delay: 500,
+    duration: 1000,
   distance:"60px",
   reset:true,
   origin: "bottom"
   });
 
   ScrollReveal().reveal('.cuadro-5', {
-    delay: 1000,
-    duration: 1200,
+    delay: 500,
+    duration: 1000,
   distance:"60px",
   reset:true,
   origin: "bottom"
   });
 
   ScrollReveal().reveal('.cuadro-6', {
-    delay: 1300,
-    duration: 1200,
+    delay: 500,
+    duration: 1000,
   distance:"60px",
   reset:true,
   origin: "bottom"
@@ -80,31 +80,31 @@ ScrollReveal().reveal('.button', {
 
   ScrollReveal().reveal('.tu_mejor_opcion', {
     delay: 300,
-    duration: 1200,
+    duration: 1000,
   distance:"60px",
   reset:true,
   origin: "top"
   });
 
   ScrollReveal().reveal('.otro-color', {
-    delay: 900,
-    duration: 1200,
+    delay: 500,
+    duration: 1000,
   distance:"60px",
   reset:true,
   origin: "bottom"
   });
 
   ScrollReveal().reveal('.icon', {
-    delay: 1100,
-    duration: 1500,
+    delay: 400,
+    duration: 1000,
   distance:"0px",
   reset:true,
   origin: "top"
   });
 
   ScrollReveal().reveal('.material-icons', {
-    delay: 1700,
-    duration: 1500,
+    delay: 400,
+    duration: 1000,
   distance:"0px",
   reset:true,
   origin: "top",
